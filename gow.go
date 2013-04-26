ffd21456d18121a458bff3b0b618b66c1b3f2f3d
s. rannou <mxs@sbrk.org>
2013-04-26 22:22:09 +0200

    Added static and dynamic handlers with a cache for templates.

--- gow.go
+++ gow.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
-	"net/http"
 	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"html/template"
 	"io/ioutil"
 	"log"
+	"net/http"
 )
 
 // Config contains the various configurations of our project
@@ -13,23 +15,50 @@ type Config struct {
 	ListenOn string // Interface to listen on
 }
 
-// MyHandlerFunc serves a http request
-type MyHandlerFunc func(*mux.Router, http.ResponseWriter, *http.Request)
-
-// HomeHandler is a MyHandlerFunc that serves the home page
-func HomeHandler(*mux.Router, http.ResponseWriter, *http.Request) {
-
+// DynamicHandlerFuncParams holds parameters dedicated to a page handler
+type DynamicHandlerFuncParams struct {
+	Router   *mux.Router        // Router of the request
+	Template *template.Template // Template of the dynamic page
 }
 
-// BuildHandler maps MyHandlerFunc to http.HandlerFunc
-func BuildHandler(router *mux.Router, handler MyHandlerFunc) http.HandlerFunc {
-	return func (w http.ResponseWriter, r *http.Request) {
-		handler(router, w, r)
+// Gow holds variables common to all handlers
+type Gow struct {
+	Config    *Config                       // Config holds the configuration of Gow
+	Templates map[string]*template.Template // Templates is a cache of parsed templates
+	Router    *mux.Router                   // Router to all requests	
+}
+
+// DynamicHandlerFunc serves a http request
+type DynamicHandlerFunc func(*DynamicHandlerFuncParams, http.ResponseWriter, *http.Request)
+
+// HomeHandler is a DynamicHandlerFunc that serves the home page
+func HomeHandler(p *DynamicHandlerFuncParams, w http.ResponseWriter, r *http.Request) {
+	p.Template.Execute(w, nil)
+}
+
+// BuildHandler maps DynamicHandlerFunc to http.HandlerFunc
+func (g *Gow) BuildDynamicdHandler(handler DynamicHandlerFunc, path string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl, in_cache := g.Templates[path]
+		if !in_cache {
+			new_tpl, err := template.ParseFiles(fmt.Sprintf("www/dynamic/%s", path))
+			if err != nil {
+				log.Printf("unable to parse template %s: %s", path, err)
+			}
+			g.Templates[path] = new_tpl
+			tpl = new_tpl
+		}
+		if tpl != nil {
+			log.Printf("serving template %s", path)
+			handler(&DynamicHandlerFuncParams{g.Router, tpl}, w, r)
+		} else {
+			log.Printf("template %s wasn't created", path)
+		}
 	}
 }
 
-// NewConfig creates a new configuration from the input path
-func NewConfig(path string) *Config {
+// NewGow creates a new Gow from a configuration path
+func NewGow(path string) *Gow {
 	file, err := ioutil.ReadFile(path)
 	if err != nil {
 		log.Fatalf("config error: %v", err)
@@ -39,13 +68,20 @@ func NewConfig(path string) *Config {
 	if err != nil {
 		log.Fatalf("config error: %v", err)
 	}
-	return &cfg
+
+	r := mux.NewRouter()
+
+	return &Gow{
+		&cfg,
+		make(map[string]*template.Template),
+		r,
+	}
 }
 
 func main() {
-	cfg := NewConfig("gow.json")
-	r := mux.NewRouter()
-	r.HandleFunc("/", BuildHandler(r, HomeHandler))
-	http.Handle("/", r)
-	log.Fatal(http.ListenAndServe(cfg.ListenOn, nil))
+	gow := NewGow("gow.json")
+	gow.Router.HandleFunc("/", gow.BuildDynamicdHandler(HomeHandler, "home.html"))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("www/static"))))
+	http.Handle("/", gow.Router)
+	log.Fatal(http.ListenAndServe(gow.Config.ListenOn, nil))
 }
